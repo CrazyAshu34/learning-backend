5742f91b82ebca572750c7af2b3b40da6f22eaf9
suruchi810 <soni.s62668@gmail.com>
2026-06-10 16:53:37 +0530

    register user is done

--- src/config/db.js
+++ src/config/db.js
@@ -2,4 +2,11 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("./test.db");
 
+db.run(`CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NULL, 
+        password TEXT NOT NULL
+    )`);
+
 export default db;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,2 +1,37 @@
-export const register = () => {};
-export const login = () => {};
+import db from "../config/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// create user
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    db.run(
+      `INSERT INTO users (name, email, password) VALUES(?,?,?)`,
+      [name, email, hashPassword],
+      (e) => {
+        if (e) {
+          console.log(e);
+          return res.status(400).json({
+            message: "User already exists",
+          });
+        } else {
+          res.status(201).json({
+            message: "User registered successfully",
+          });
+        }
+      },
+    );
+
+    console.log(name, email, password);
+  } catch (e) {
+    console.error("registration error:, ", e.message);
+    res
+      .status(500)
+      .json({ message: "user cannot register", message: e.message });
+  }
+};
+// export const login = () => {};
