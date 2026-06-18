e0cfddc583bc5c6841c8db9403528021e257e625
suruchi810 <soni.s62668@gmail.com>
2026-06-18 18:30:07 +0530

    done changes

--- src/modules/customers/customer.js
+++ src/modules/customers/customer.js
@@ -1,11 +1,10 @@
 import db from "../../config/db.js";
 import { v4 as uuidv4 } from "uuid";
 // import { isMissing } from "../../utils.js";
-const uuid = uuidv4();
 
 db.run(
   `CREATE TABLE IF NOT EXISTS customers (
-    id TEXT PRIMARY KEY DEFAULT (uuid),
+    id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     phone TEXT,
@@ -21,14 +20,52 @@ db.run(
 
 export const createCustomer = async (req, res) => {
   try {
-    const { name, email, phone, dob, assignAgent, actionStage } = req.body;
-    console.log(name, email, phone, dob, assignAgent, actionStage);
+    const id = uuidv4();
 
-    res
-      .status(200)
-      .json({ data: name, email, phone, dob, assignAgent, actionStage });
+    const { name, email, phone, dob, assignAgent, actionStage } = req.body;
+
+    db.run(
+      `INSERT INTO customers (id, name, email, phone, dob, assignAgent, actionStage) VALUES (?,?,?,?,?,?,?)`,
+      [id, name, email, phone, dob, assignAgent, actionStage],
+      function (err) {
+        if (err) {
+          console.error("Insert error:", err);
+          return res
+            .status(400)
+            .json({ message: "Customer not created", error: err.message });
+        }
+
+        return res.status(201).json({
+          message: "Customer created",
+          data: {
+            id,
+            name,
+            email,
+            phone,
+            dob,
+            assignAgent,
+            actionStage,
+          },
+        });
+      },
+    );
   } catch (error) {
-    console.error("error in getting role");
-    res.status(400).json({ message: "error users" });
+    console.error("Error in createCustomer:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
+
+export const getAllCustomer = async (req, res) =>{
+      try{
+        db.all(`
+          SELECT * FROM customers
+          `, (e)=>{
+           if(e){
+            console.log(e)
+            return 
+           }
+          })
+      }
+}
